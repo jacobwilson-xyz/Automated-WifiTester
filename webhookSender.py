d3d7875c5623dc4b0592f4ce38626b26da515874
Jacob <jjwsmail@gmail.com>
2024-03-05 15:51:34 +0000

    Add test webhooks in as their own function

--- webhookSender.py
+++ webhookSender.py
@@ -10,7 +10,7 @@ def sendWebhookNotification(ping, downloadSpeed, uploadSpeed, currentTime, elaps
         "summary": f"⚡ Speedtest Data - {hostname} via {networkSSID}",
         "sections": [{
             "activityTitle": f"⚡ Speedtest Data - {hostname} via {networkSSID}",
-            "activitySubtitle": "Open for all info",
+            "activitySubtitle": f"[{config.uniqueIdentifer}] - Open for data ouput",
             "activityImage": "https://www.newton.ac.uk/wp-content/uploads/2023/12/ini-socmed-green-white-graphic.png",
             "facts": [{
                 "name": "💓 Ping",
@@ -42,7 +42,7 @@ def sendWebhookNotification(ping, downloadSpeed, uploadSpeed, currentTime, elaps
     }
 
     requests.post(config.webhookURL, data=json.dumps(payload))
-    print("Request Sent.")
+    print("Request Sent (Windows).")
     return
 
 def sendWebhookNotificationLinux(ping, downloadSpeed, uploadSpeed, currentTime, elapsedTime, serverName, serverLocation, hostname, networkSSID, physicalLocation, wlanFreq, wlanRate, wlanQualityPercent, wlanSignalStrength):
@@ -53,7 +53,7 @@ def sendWebhookNotificationLinux(ping, downloadSpeed, uploadSpeed, currentTime, 
         "summary": f"⚡ Speedtest Data - {hostname} via {networkSSID}",
         "sections": [{
             "activityTitle": f"⚡ Speedtest Data - {hostname} via {networkSSID}",
-            "activitySubtitle": "Open for all info",
+            "activitySubtitle": f"[{config.uniqueIdentifer}] - Open for data ouput",
             "activityImage": "https://www.newton.ac.uk/wp-content/uploads/2023/12/ini-socmed-green-white-graphic.png",
             "facts": [{
                 "name": "📡 Frequency",
@@ -91,6 +91,116 @@ def sendWebhookNotificationLinux(ping, downloadSpeed, uploadSpeed, currentTime, 
             }, {
                 "name": "🏢 Physical Location",
                 "value": f"{physicalLocation}"
+            }],
+            "markdown": "true"
+        }]
+    }
+
+    requests.post(config.webhookURL, data=json.dumps(payload))
+    print("Request Sent (Linux)")
+    return
+
+def sendTestWebhook(ping, downloadSpeed, uploadSpeed, currentTime, elapsedTime, serverName, serverLocation, hostname, networkSSID, physicalLocation, ip):
+    payload = {
+        "@type": "MessageCard",
+        "@context": "http://schema.org/extensions",
+        "themeColor": "0076D7",
+        "summary": f"🟢 Device Online: '{hostname}' [{config.systemPlatform}]",
+        "sections": [{
+            "activityTitle": f"🟢 Device Online: '{hostname}'",
+            "activitySubtitle": f"[{config.uniqueIdentifer}] - Open for data ouput",
+            "activityImage": "https://www.newton.ac.uk/wp-content/uploads/2023/12/ini-socmed-green-white-graphic.png",
+            "facts": [{
+                "name": "💓 Ping",
+                "value": f"{ping}"
+            },{
+                "name": "⬇️ Download",
+                "value": f"{downloadSpeed}"
+            }, {
+                "name": "⬆️ Upload",
+                "value": f"{uploadSpeed}"
+            }, {
+                "name": "🕒 Time",
+                "value": f"{currentTime}"
+            }, {
+                "name": "⏱️ Elapsed",
+                "value": f"{elapsedTime}"
+            }, {
+                "name": "🌐 SSID",
+                "value": f"{networkSSID}"
+            },{
+                "name": "🖥️ Host",
+                "value": f"{serverName}"
+            }, {
+                "name": "🌍 Country",
+                "value": f"{serverLocation}"
+            }, {
+                "name": "🏢 Physical Location",
+                "value": f"{physicalLocation}"
+            }, {
+                "name": "🏢 IP Address",
+                "value": f"{ip}"
+            }],
+            "markdown": "true"
+        }]
+    }
+
+    requests.post(config.webhookURL, data=json.dumps(payload))
+    print("Request Sent (Windows)")
+    return
+
+def sendTestWebhookLinux(ping, downloadSpeed, uploadSpeed, currentTime, elapsedTime, serverName, serverLocation, hostname, networkSSID, physicalLocation, wlanFreq, wlanRate, wlanQualityPercent, wlanSignalStrength, ip):
+    payload = {
+        "@type": "MessageCard",
+        "@context": "http://schema.org/extensions",
+        "themeColor": "0076D7",
+        "summary": f"🟢 Device Online: '{hostname}'",
+        "sections": [{
+            "activityTitle": f"🟢 Device Online: '{hostname}' [{config.systemPlatform}]",
+            "activitySubtitle": f"[{config.uniqueIdentifer}] - Open for data ouput",
+            "activityImage": "https://www.newton.ac.uk/wp-content/uploads/2023/12/ini-socmed-green-white-graphic.png",
+            "facts": [{
+                "name": "📡 Frequency",
+                "value": f"{wlanFreq}"
+            }, {
+                "name": "⏩ Bitrate",
+                "value": f"{wlanRate}"
+            },{
+                "name": "📈 Signal Quality",
+                "value": f"{wlanQualityPercent}%"
+            },{
+                "name": "📶 Signal Strength",
+                "value": f"{wlanSignalStrength}"
+            },{
+                "name": "💓 Ping",
+                "value": f"{ping}"
+            },{
+                "name": "⬇️ Download",
+                "value": f"{downloadSpeed}"
+            }, {
+                "name": "⬆️ Upload",
+                "value": f"{uploadSpeed}"
+            }, {
+                "name": "🕒 Time",
+                "value": f"{currentTime}"
+            }, {
+                "name": "⏱️ Elapsed",
+                "value": f"{elapsedTime}"
+            }, {
+                "name": "🌐 SSID",
+                "value": f"{networkSSID}"
+            },{
+                "name": "🖥️ Host",
+                "value": f"{serverName}"
+            }, {
+                "name": "🌍 Country",
+                "value": f"{serverLocation}"
+            }, {
+                "name": "🏢 Physical Location",
+                "value": f"{physicalLocation}"
+            }, {
+                "name": "🏢 IP Address",
+                "value": f"{ip}"
             }],
             "markdown": "true"
         }]
